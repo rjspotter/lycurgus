4cf961328a130f28791b442c59eaa2558093597c
Randall Potter <rjspotter@intentionally-blank.com>
2016-04-09 15:22:10 -0400

    can spend seeds for planting

--- src/lycurgus/core.clj
+++ src/lycurgus/core.clj
@@ -45,12 +45,16 @@
       (clojure.core/= true
                   (if (:result res#) res# true)))))
 
+(def double-except-nan (gen/such-that #(not (Double/isNaN %)) gen/double))
+
 (with-test
   (defconstrainedfn sqr
-    [n] [number? => number? (>= % 0) (= (math/abs n) (math/sqrt %))]
+    [n] [number? (not (Double/isNaN n))=> number? (>= % 0) (= (math/abs n) (math/sqrt %))]
     (* n n))
   
-  (predict-to [v (gen/one-of [gen/int])] (sqr v))
+  (predict-to [v (gen/one-of [gen/int
+                              double-except-nan])] (sqr v))
+  
   (predict-not [v (gen/one-of [gen/keyword
                            gen/boolean
                            (gen/vector gen/int)
@@ -61,9 +65,12 @@
 ; An acre takes one bushel to seed.
 (defrecord City [grain citizens land])
 
-(defrecord Spend [feed seed buy]) ; spends are in bushels
+; spends are in bushels
+(defrecord Spend [feed seed warfare])
 
-(defrecord Army [pillage conquer]) ; Cost to conquer in bushels ; Amount gained by pillage and burning occupied land
+; Cost to conquer in bushels
+; Amount gained by pillage and burning occupied land
+(defrecord Army [pillage conquer])
 
 (def *evil-root* (atom (list (->City 2800 100 1000))))
 
@@ -75,17 +82,39 @@
     [city spend]
     [(every? integer?  [(:feed spend) (:grain city) (:citizens city)])
      (every? #(>= % 0) [(:feed spend) (:grain city) (:citizens city)])
+     (<= (:feed spend) (:grain city))
      =>
      (not (nil? %))
      (<= (:grain %)    (:grain city))
      (<= (:citizens %) (:citizens city))
      (=  (:land %)     (:land city))
-     (every? integer? [(:grain %) (:citizens %)])
-     (every? #(>= % 0) [(:grain %) (:citizens %)])
+     (every? integer? [(:grain %) (:citizens %) (:land %)])
+     (every? #(>= % 0) [(:grain %) (:citizens % (:land %))])
      ]
     (let [f (:feed spend)
           g (:grain city)
           c (:citizens city)
           s (int (/ f 20))]
       (->City (- g f) (min c s) (:land city))
+      ))
+  )
+
+(with-test
+  (defconstrainedfn seed
+    "Sow the seeds for next years crop"
+    [city spend]
+    [(every? integer?  [(:seed spend) (:grain city) (:citizens city)])
+     (every? #(>= % 0) [(:seed spend) (:grain city) (:citizens city)])
+     (<= (:seed spend) (:grain city))
+     =>
+     (not (nil? %))
+     (<= (:grain %)    (:grain city))
+     (=  (:citizens %) (:citizens city))
+     (=  (:land %)     (:land city))
+     (every? integer? [(:grain %) (:citizens %) (:land %)])
+     (every? #(>= % 0) [(:grain %) (:citizens % (:land %))])
+     ]
+    (let [s (:seed spend)
+          g (:grain city)]
+      (->City (- g s) (:citizens city) (:land city))
     )))
