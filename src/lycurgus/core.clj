2da2895f4e352c1d80d0a3ec317b31e4020d88c8
R. Potter <rjspotter@gmail.com>
2016-06-18 18:24:13 -0700

    2016.06.18

--- src/lycurgus/core.clj
+++ src/lycurgus/core.clj
@@ -51,10 +51,9 @@
   (defconstrainedfn sqr
     [n] [number? (not (Double/isNaN n))=> number? (>= % 0) (= (math/abs n) (math/sqrt %))]
     (* n n))
-  
-  (predict-to [v (gen/one-of [gen/int
-                              double-except-nan])] (sqr v))
-  
+
+  (predict-to [v (gen/one-of [gen/int])] (sqr v))
+
   (predict-not [v (gen/one-of [gen/keyword
                            gen/boolean
                            (gen/vector gen/int)
@@ -66,15 +65,23 @@
 (defrecord City [grain citizens land])
 
 ; spends are in bushels
-(defrecord Spend [feed seed warfare])
+(defrecord Spend [feed seed conquor pilliage])
 
 ; Cost to conquer in bushels
 ; Amount gained by pillage and burning occupied land
 (defrecord Army [pillage conquer])
 
-(def *evil-root* (atom (list (->City 2800 100 1000))))
+;;(def *evil-root* (ref (list (->City 2800 100 1000))))
 
 (defn now [] (first *evil-root*))
+
+(def valid-turn (gen/let
+                    [grain gen/pos-int citizens gen/pos-int land gen/pos-int exchange gen/pos-int]
+                  {:city  (->City grain citizens land)
+                   :spend (->Spend (int (/ grain 20)) (min grain land) (int (/ grain (+ 1 exchange))) 0)
+                   :army  (->Army exchange exchange)
+                   }
+                  ))
 
 (with-test
   (defconstrainedfn feed
@@ -95,13 +102,13 @@
           g (:grain city)
           c (:citizens city)
           s (int (/ f 20))]
-      (->City (- g f) (min c s) (:land city))
+      (assoc city :grain (- g f) :citizens (min c s))
       ))
-  )
+  (predict-to [t valid-turn] (feed (:city t) (:spend t))))
 
 (with-test
-  (defconstrainedfn seed
-    "Sow the seeds for next years crop"
+  (defconstrainedfn sow
+    "Sow the seeds for next year's crop"
     [city spend]
     [(every? integer?  [(:seed spend) (:grain city) (:citizens city)])
      (every? #(>= % 0) [(:seed spend) (:grain city) (:citizens city)])
@@ -117,4 +124,30 @@
     (let [s (:seed spend)
           g (:grain city)]
       (->City (- g s) (:citizens city) (:land city))
-    )))
+      ))
+  (predict-to [t valid-turn] (feed (:city t) (:spend t))))
+
+(with-test
+  (defconstrainedfn conquor
+    "Conquer : feeding the army extra so they can effectively kill their neighbors and take their land"
+    [city spend army]
+    [(every? integer?  [(:warfare spend) (:grain city) (:citizens city)])
+     (every? #(>= % 0) [(:warfare spend) (:grain city) (:citizens city)])
+     (<= (:warfare spend) (:grain city))
+     =>
+     (not (nil? %))
+     (<= (:grain %)    (:grain city))
+     (=  (:citizens %) (:citizens city))
+     (>= (:land %)     (:land city))
+     (every? integer? [(:grain %) (:citizens %) (:land %)])
+     (every? #(>= % 0) [(:grain %) (:citizens % (:land %))])
+     ]
+    (let [spent (:warfare spend)
+          grain (:grain city)
+          cost  (:conquor army)
+          ]
+      (->City (- grain spent) (:citizens city) (+ (:land city) (int (Math/floor (/ spent cost)))))
+      ))
+  (predict-to [t valid-turn] (feed (:city t) (:spend t) (:army t))))
+
+
